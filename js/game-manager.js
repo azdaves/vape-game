cc8b220e7a5f64f273aca03dbc59218a15448c4c
Dave Schilling <ds@daves-mbp.wusd.local>
2018-11-01 09:49:43 -0700

    changes to students and devices

--- js/game-manager.js
+++ js/game-manager.js
@@ -14,10 +14,62 @@ let GameManager = {
       case "Cheer":
         student = new Student(classType, 200, 120, 90, 50);
         break;
+    }
+    let getInterface = document.querySelector(".interface");
+    getInterface.innerHTML =
+      '<img src="images/avatar-players/' +
+      classType.toLowerCase() +
+      '.png" class="img-avatar"><div><h3>' +
+      classType +
+      "</h3><p class="health-student">Health: " +
+      student.health +
+      "</p><p>Lungs: " +
+      student.lungs +
+      "</p><p>Strength: " +
+      student.strength +
+      "</p><p>Brain: " +
+      student.brain +
+      "</p></div>";
 
-      default:
+      class="health-student"
+  },
+  setPreFight: function() {
+    let getHeader = document.querySelector(".header");
+    let getActions = document.querySelector(".actions");
+    let getLounge = document.querySelector(".lounge");
+    getHeader.innerHTML = "<p>Task: Find A Device!</p>";
+    getActions.innerHTML =
+      '<a href="#" class="btn-prehit" onclick="GameManager.setFight()">Search For A Device!</a>';
+    getLounge.style.visibility = "visible";
+  },
+  setFight: function() {
+    let getHeader = document.querySelector(".header");
+    let getActions = document.querySelector(".actions");
+    let getDevice = document.querySelector(".device");
+    //create vape device
+    let device00 = new Device("Juul", 100, 25, 50, 50);
+    let device01 = new Device("Drop", 90, 35, 70, 80);
+    let chooseRandomDevice = Math.floor(Math.random() * Math.floor(2));
+    switch (chooseRandomDevice) {
+      case 0:
+        device = device00;
+
+        break;
+      case 1:
+        device = device01;
         break;
     }
-  },
-  setPreFight: function() {}
+    getHeader.innerHTML = "<p>Task: Hit or Walk Away</p>";
+    getActions.innerHTML =
+      '<a href="#" class="btn-prehit" onclick="StudentMoves.calcDamage()">Use Device?</a>';
+    getDevice.innerHTML =
+      '<img src="images/avatar-devices/' +
+      device.deviceType.toLowerCase() +
+      '.jpeg" alt="' +
+      device.deviceType +
+      '" class="img-avatar"><div><h3>' +
+      device.deviceType +
+      "</h3><p="health-device">Health: ' + device.health + '</p><p>Lungs: ' + device.lungs + '</p><p>Strength: ' + device.strength + '</p><p>Brain: ' + device.brain + '</p></div>';
+  }
+
 };
--- js/student.js
+++ js/student.js
@@ -7,3 +7,45 @@ function Student(classType, health, lungs, strength, brain) {
   this.strength = strength;
   this.brain = brain;
 }
+
+let StudentMoves = {
+  calcAttack: function() {
+    //who attack first
+    let getPlayerBrain = player.brain;
+    let getDeviceBrain = device.brain;
+  }
+  //player attacks - hits vape
+  let studentAttack = function() {
+    let calcBaseDamage;
+    if(player.lungs > 0) {
+        calcBaseDamage = student.strength * student.lungs / 1000;
+    } else {
+        calcBaseDamage = student.strength * student.health / 1000;
+    }
+    let offsetDamage = Math.floor(Math.random() * Math.floor(10));
+    let calcOutputDamage = calcBaseDamage + offsetDamage;
+    //number of hits
+    let numberOfHits = Math.floor(Math.random() * Math.floor(student.health / 10) / 2) + 1;
+    let attackValues = [calcOutputDamage, numberOfHits];
+    return attackValues;
+  }
+
+    //device attacks - hits vape
+    let deviceAttack = function() {
+        let calcBaseDamage;
+        if(device.lungs > 0) {
+            calcBaseDamage = device.strength * device.lungs / 1000;
+        } else {
+            calcBaseDamage = device.strength * device.health / 1000;
+        }
+        let offsetDamage = Math.floor(Math.random() * Math.floor(10));
+        let calcOutputDamage = calcBaseDamage + offsetDamage;
+        //number of hits
+        let numberOfHits = Math.floor(Math.random() * Math.floor(device.health / 10) / 2) + 1;
+        let attackValues = [calcOutputDamage, numberOfHits];
+        return attackValues;
+      }
+      
+      let getStudentHealth = document.querySelector(".health-student");
+      let getDeviceHealth = document.querySelector(".health-device");
+};
